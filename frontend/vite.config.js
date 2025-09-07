570711c5d6a98bd1faac91f54fc342c9d4521047
Kevin Agustin Ruiz <Kevinagustinrockz@gmail.com>
2025-09-07 14:53:02 -0300

    change CORS and .env connections to /api connect

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,9 +5,13 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
+    port: 8080,
     proxy: {
-      "/api": { target: "http://127.0.0.1:8000", changeOrigin: true },
+      "/api": {
+        target: "http://api:8000", // nombre del servicio docker de la API
+        changeOrigin: true,
+        secure: false,
+      },
     },
     headers: {
       "Content-Security-Policy": [
