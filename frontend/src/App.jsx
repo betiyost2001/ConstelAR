187c852b65487c8967c8cd9cd17eeb250e0c55e2
Kevin Agustin Ruiz <Kevinagustinrockz@gmail.com>
2025-10-04 13:50:42 -0300

    añadido tailwindcss

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,6 @@ import Header from "./components/Header";
 import FilterBar from "./components/FilterBar";
 import MapView from "./components/MapView";
 import Legend from "./components/Legend";
-import "./App.css";
 import { DEFAULT_POLLUTANT } from "./constants/pollutants";
 
 export default function App() {
